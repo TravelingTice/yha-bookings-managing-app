5d7c9707bbcfc48a0c9b604561b1fb6cd934ef9f
Tice Kralt <34274090+TravelingTice@users.noreply.github.com>
2019-07-10 12:34:35 +0800

    Fix header with active state

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -7,12 +7,18 @@ class Header extends Component {
     super(props)
 
     this.state = {
-      active: 'Home'
+      active: '/'
     }
   }
-    updatePage(page) {
+    componentDidMount() {
       this.setState({
-        active: page
+        active: window.location.pathname
+      });
+    }
+
+    updatePage(url) {
+      this.setState({
+        active: url
       });
     }
 
@@ -22,28 +28,28 @@ class Header extends Component {
         <header>
         <nav>
           <Link 
-          onClick={e => this.updatePage('Home')}
-          className={active === 'Home' ? 'active' : ''} 
+          onClick={e => this.updatePage('/')}
+          className={active === '/' ? 'active' : ''} 
           to="/">Home</Link>
           <Link 
-          onClick={e => this.updatePage('PersonIsPaying')}
-          className={active === 'PersonIsPaying' ? 'active' : ''} 
+          onClick={e => this.updatePage('/person-is-paying')}
+          className={active === '/person-is-paying' ? 'active' : ''} 
           to="/person-is-paying">Person is paying</Link>
           <Link 
-          onClick={e => this.updatePage('NewCheckIn')}
-          className={active === 'NewCheckIn' ? 'active' : ''} 
+          onClick={e => this.updatePage('/new-check-in')}
+          className={active === '/new-check-in' ? 'active' : ''} 
           to="/new-check-in">New Check In</Link>
           <Link 
-          onClick={e => this.updatePage('CheckOut')}
-          className={active === 'CheckOut' ? 'active' : ''} 
+          onClick={e => this.updatePage('/check-out')}
+          className={active === '/check-out' ? 'active' : ''} 
           to="/check-out">Check Out</Link>
           <Link 
-          onClick={e => this.updatePage('LookUpPerson')}
-          className={active === 'LookUpPerson' ? 'active' : ''} 
+          onClick={e => this.updatePage('/look-up')}
+          className={active === '/look-up' ? 'active' : ''} 
           to="/look-up">Look Up</Link>
           <Link 
-          onClick={e => this.updatePage('SingleRoomList')}
-          className={active === 'SingleRoomList' ? 'active' : ''} 
+          onClick={e => this.updatePage('/single-room-list')}
+          className={active === '/single-room-list' ? 'active' : ''} 
           to="/single-room-list">Single Room List</Link>
         </nav>
     
