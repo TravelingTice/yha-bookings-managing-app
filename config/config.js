565e996076d0304841e4f504d2f5420a045cfd02
Tice Kralt <34274090+TravelingTice@users.noreply.github.com>
2019-07-24 11:38:47 +0800

    Version 1.0.1

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb+srv://tice_kralt:DgWQqwMQs3U_hKE@cluster0-mdylm.mongodb.net/test/YHAManagingApp',
+  db_dev: 'mongodb://localhost:27017/managingAppDB',
+};
